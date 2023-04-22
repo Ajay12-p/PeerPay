eee63019e95fd08f4c903ad2e78785c6c8f787d4
Ajay krishnanand Paikra <75928188+Ajay12-p@users.noreply.github.com>
2023-04-22 10:19:36 +0530

    Create page.js

---
+++ client/src/Navbar/page.js
@@ -0,0 +1,57 @@
+'use client';
+import  styles from "./page.module.scss"
+import Spline from '@splinetool/react-spline';
+import { useNavigate } from "react-router-dom";
+const Navbar = () => {
+    const navigate = useNavigate()
+    const a =() =>{
+        navigate("./dashbord")
+    }
+     return (
+        <>
+            <div  className={styles.main}>
+            <div >
+                <div style={{ marginTop:"30px"}} >
+                     <h1 className={styles.heading}>
+                     Limit-Less
+                     </h1>
+           
+{/* <Spline scene="https://prod.spline.design/DjIcmxwy1aGaTtUO/scene.splinecode"/> */}
+                </div>
+                </div>
+            
+            
+           
+
+             <div >
+              
+                 <div style={{display:"flex"  }}>
+               
+               
+                <h3  className={styles.text} onClick={()=>{
+                    a();
+                }} >
+                    Dashbord
+                </h3>
+
+                <h3  className={styles.text}>
+                    Homepage
+                </h3>
+                   
+                </div>
+            </div>
+           
+         
+            <div className={styles.button}>
+           GET STARTED...
+               
+            </div>
+            
+           
+            
+            </div>
+        </>
+    )
+}
+
+export default Navbar;
