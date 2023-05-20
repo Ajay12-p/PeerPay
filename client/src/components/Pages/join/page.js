49327648306a3c7c6806d5727b32339715510b19
Trymbak Mahant <trymbak0102mahant@gmail.com>
2023-05-20 20:57:22 +0530

    commit

--- client/src/components/Pages/join/page.js
+++ client/src/components/Pages/join/page.js
@@ -1,12 +1,13 @@
-import { Button, Center, Flex } from "@chakra-ui/react";
+import { Accordion, Button, Center, Flex } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../Navbar/page";
+import "./page.module.css";
 import Submit from "../../../UI/Buttons/Submit/Submit";
 import { useDisclosure } from "@chakra-ui/react";
 import Spacebutton from "../../../UI/Buttons/spacebutton/Spacebutton";
 import { useState, useEffect } from "react";
 import ConfettiExplosion from "react-confetti-explosion";
-
+import axios from "axios";
 import {
   Modal,
   ModalOverlay,
@@ -26,116 +27,148 @@ import {
 } from "@chakra-ui/react";
 const Join = () => {
   const [flagofcong, setFlagofcong] = useState(true);
-  const [windowDimensions, setWindowDimensions] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
+  const [data, setData] = useState({
+    AccountAdress: "",
+    BussinessName: "",
   });
-  useEffect(() => {
-    window.addEventListener("resize", detectResize);
-    return () => {
-      window.removeEventListener("resize", detectResize);
-    };
-  }, []);
-  const detectResize = () => {
-    setWindowDimensions({
-      width: window.innerWidth,
-      height: window.innerHeight,
-    });
-  };
+
   const [flags, setFlags] = useState(false);
   const navigate = useNavigate();
-  const HandelSubmit = () => {
+
+  const HandelSubmit = async () => {
+    console.log(data.AccountAdress.length);
+    // if (data.AccountAdress.length != 42) {
+    //   alert("Please Enter a valid Account Address");
+    //   return;
+    // }
+    // if (data.BussinessName.length < 3) {
+    //   alert("Please Enter a valid Bussiness Name");
+    //   return;
+    // }
+
+    // const response = await axios.post(
+    //   "http://localhost:5000/api/Bussiness/register",
+    //   data
+    // );
+
+    // console.log(response);
     setFlagofcong(false);
     console.log("heleof");
     setFlags(true);
-    // setTimeout(() => {
-    //   navigate("/ThankYOU");
-    // }, 8000);
   };
   return (
     <div className="Mainbody">
       <Navbar />
 
-      <Center pt="25vh" pb="25vh">
-        {/* <Flex> */}
-        <Card
-          background=" linear-gradient(to right, rgb(199, 210, 254), rgb(254, 202, 202), rgb(254, 249, 195))"
-          position="Cetner"
-          width={"30%"}
-          height={"400px"}
-        >
-          {flagofcong ? (
-            <Box position="relative" padding={20}>
-              <Box>
-                <FormControl isRequired>
-                  <FormLabel>Enter the Address...</FormLabel>
-                  <Input width={80} height={20} placeholder="0x...." />
-                </FormControl>
-              </Box>
-              <Box>
-                <FormControl isRequired>
-                  <FormLabel>companey Name</FormLabel>
-                  <Input width={80} height={20} placeholder="Enter the Name" />
-                </FormControl>
-              </Box>
+      {!flagofcong ? (
+        <Center pt="25vh" pb="25vh">
+          {/* <Flex> */}
+          <Card
+            background=" linear-gradient(to right, rgb(199, 210, 254), rgb(254, 202, 202), rgb(254, 249, 195))"
+            position="Cetner"
+            width={"30%"}
+            height={"400px"}
+          >
+            {!flagofcong ? (
+              <Box position="relative" padding={20}>
+                <Box>
+                  <FormControl isRequired>
+                    <FormLabel>Enter the Address...</FormLabel>
+                    <Input
+                      width={80}
+                      height={20}
+                      name="AccountAdress"
+                      onChange={(e) => {
+                        setData({ ...data, AccountAdress: e.target.value });
+                      }}
+                      placeholder="0x...."
+                    />
+                  </FormControl>
+                </Box>
+                <Box>
+                  <FormControl isRequired>
+                    <FormLabel>companey Name</FormLabel>
+                    <Input
+                      width={80}
+                      name="BussinessName"
+                      onChange={(e) => {
+                        setData({ ...data, BussinessName: e.target.value });
+                      }}
+                      height={20}
+                      placeholder="Enter the Name"
+                    />
+                  </FormControl>
+                </Box>
 
-              <Box
-                style={{
-                  marginTop: "5vh",
-                }}
-              >
-                <Submit
-                  Name="Submit 😀"
-                  Nextname="Lets Go 🚀"
-                  Function={HandelSubmit}
-                />
-              </Box>
-            </Box>
-          ) : (
-            <Box position="relative" paddingTop={"29%"}>
-              <h1
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-
-                  fontSize: "40px",
-                  fontWeight: "bold",
-                  width: "100%",
-                }}
-              >
-                Congrats{" "}
-                {flags && (
-                  <ConfettiExplosion
-                    force={0.8}
-                    duration={8000}
-                    particleCount={300}
-                    width={1600}
-                  />
-                )}
-                🎊🎊🥳
-              </h1>
-
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  marginTop: "5vh",
-                }}
-              >
-                <button
-                  className="SimpbleBtn"
-                  onClick={() => {
-                    navigate("/dashbord");
+                <Box
+                  style={{
+                    marginTop: "5vh",
                   }}
                 >
-                  Dashboard
-                </button>
-              </div>
-            </Box>
-          )}
-        </Card>
-        {/* </Flex> */}
-      </Center>
+                  <Submit
+                    Name="Submit 😀"
+                    Nextname="Lets Go 🚀"
+                    Function={HandelSubmit}
+                  />
+                </Box>
+              </Box>
+            ) : (
+              <div>congratulation</div>
+              // <Box position="relative" paddingTop={"29%"}>
+              //   <h1
+              //     style={{
+              //       display: "flex",
+              //       justifyContent: "center",
+
+              //       fontSize: "40px",
+              //       fontWeight: "bold",
+              //       width: "100%",
+              //     }}
+              //   >
+              //     Congrats{" "}
+              //     {flags && (
+              //       <ConfettiExplosion
+              //         force={0.8}
+              //         duration={10000}
+              //         particleCount={300}
+              //         width={1600}
+              //       />
+              //     )}
+              //     🎊🎊🥳
+              //   </h1>
+
+              //   <div
+              //     style={{
+              //       display: "flex",
+              //       justifyContent: "center",
+              //       marginTop: "5vh",
+              //     }}
+              //   >
+              //     <button
+              //       className="SimpbleBtn"
+              //       onClick={() => {
+              //         navigate("/dashbord");
+              //       }}
+              //     >
+              //       Dashboard
+              //     </button>
+              //   </div>
+              // </Box>
+            )}
+          </Card>
+          {/* </Flex> */}
+        </Center>
+      ) : (
+        <div className="Congrats">
+          <ConfettiExplosion
+            force={0.8}
+            duration={10000}
+            particleCount={300}
+            width={1600}
+          />
+          congratulation
+        </div>
+      )}
     </div>
   );
 };
