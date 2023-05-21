a04f8e76830b34ae9b10ad2fa247598c7a223fda
Trymbak Mahant <trymbak0102mahant@gmail.com>
2023-05-21 23:52:57 +0530

    update error resolved

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,14 @@ import { ChakraProvider } from "@chakra-ui/react";
 import HomePage from "./components/Pages/HomePage/page";
 import Dashbord from "./components/Pages/dashboard/page";
 import Join from "./components/Pages/join/page";
-import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <Routes>
-      <Sidebar>
-        <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/dashbord" element={<Dashbord />} />
-        <Route exact path="/Create-account" element={<Join />} />
-        <Route exact path="/Create-account/dashbord" element={<Dashbord />} />
-      </Sidebar>
+      <Route exact path="/" element={<HomePage />} />
+      <Route exact path="/dashbord" element={<Dashbord />} />
+      <Route exact path="/Create-account" element={<Join />} />
+      <Route exact path="/Create-account/dashbord" element={<Dashbord />} />
     </Routes>
   );
 }
