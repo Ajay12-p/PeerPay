43ef84abfeaf9696a17da3b3572ddc8a844ee426
Ajay krishnanand Paikra <75928188+Ajay12-p@users.noreply.github.com>
2023-05-28 13:30:14 +0530

    Update page.js

--- client/src/components/Pages/dashboard/page.js
+++ client/src/components/Pages/dashboard/page.js
@@ -1,13 +1,51 @@
 import { useEffect, useState } from "react";
 import styles from "./page.module.scss";
-
+import { useContext } from "react";
 // import Sidebar from "../Navbar/page";
 import Sidebar from "../../Sidebar/Sidebar";
+import Docs from "../docs/page";
+import ApiPage from "../Api/page";
+import { userContext } from "../../context/accContext";
+
 const Dashbord = () => {
+
+ const ctx = useContext(userContext);
+ const historyFlag = ctx.sharedData.HistoryFlag;
+ const ApiFlag = ctx.sharedData.ApiFlag;
+ const DocsFlag  = ctx.sharedData.DocsFlag;
+
   return (
     <div className="BodyofDashboard">
-      <Sidebar />
-      dashboard
+      <div className={styles.div1}>
+
+      <Sidebar /> 
+      <div className={styles.a}>
+{/* 
+       { (historyFlag)&&
+        
+      
+        
+      } */}
+       
+       { (ApiFlag)&&
+       <div style={{marginLeft:"500px"}}>
+
+       <ApiPage/>
+
+         </div>
+        }
+       
+       { (DocsFlag)&&
+
+      <Docs/>
+
+
+        }
+       
+        
+      </div>
+      </div>
+
     </div>
   );
 };
