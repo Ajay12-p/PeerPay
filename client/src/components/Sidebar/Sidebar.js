f05dd6419021522103c534041bd4230a77191b8f
Ajay krishnanand Paikra <75928188+Ajay12-p@users.noreply.github.com>
2023-05-28 13:30:47 +0530

    Update Sidebar.js

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,11 +1,65 @@
-import "./Sidebar.css";
-import { motion } from "framer-motion";
-const Sidebar = ({ Children }) => {
+
+import styles from "./Sidebar.module.css"
+import { color, motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { userContext } from "../context/accContext";
+import Docs from "../Pages/docs/page";
+const Sidebar = () => {
+  const ctx = useContext(userContext);
+  const setHistoryFlag = ctx.sharedData.setHistoryFlag;
+  const setApiFlag = ctx.sharedData.setApiFlag;
+  const setDocsFlag  = ctx.sharedData.setDocsFlag;
+  const historyFlag = ctx.sharedData.HistoryFlag;
+  const ApiFlag = ctx.sharedData.ApiFlag;
+  const DocsFlag  = ctx.sharedData.DocsFlag;
+ 
+ 
   return (
-    <div className="main-container">
-      <motion.div animate={{ width: "200px" }} className="sidebar">
-        <main>{Children}</main>
-      </motion.div>
+    <div >
+      
+      <div className={styles.border1}>
+      <div className={styles.sidebar}
+      >
+
+
+
+        
+          <div className = {styles.button}>
+
+            <div> history</div>
+          </div>
+          
+        <div className = {styles.button}>
+
+<div    onClick={()=>{
+  if(ApiFlag){
+    setApiFlag(true);
+
+  }else{
+    setApiFlag(false);
+  }
+}}> Api
+</div>
+</div>
+<div className = {styles.button} >
+
+<div onClick={()=>{
+  if(DocsFlag){
+
+    setDocsFlag(true);
+  }else{
+    setDocsFlag(false)
+  }
+}}>  Docs </div>
+</div>
+   
+
+    
+
+
+    </div>
+   </div>
     </div>
   );
 };
