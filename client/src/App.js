a0c1d6033d3c39dec96e20b6b098c6a32ed6858e
Ajay krishnanand Paikra <75928188+Ajay12-p@users.noreply.github.com>
2023-05-30 00:17:48 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,10 @@ import HomePage from "./components/Pages/HomePage/page";
 import Dashbord from "./components/Pages/dashboard/page";
 import Join from "./components/Pages/join/page";
 import Payment from "./components/Pages/Payment/Payment";
+import History from "./components/Pages/History/history";
+
+
+
 
 function App() {
   return (
@@ -15,6 +19,8 @@ function App() {
       <Route exact path="/Create-account" element={<Join />} />
       <Route exact path="/Payment" element={<Payment />} />
       <Route exact path="/Create-account/dashbord" element={<Dashbord />} />
+      <Route exact path="/history" element={<History/>} />
+      
     </Routes>
   );
 }
