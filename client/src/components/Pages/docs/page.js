3872bd53c3b53de1f3f9d29d7e1b461a70805cd6
Ajay krishnanand Paikra <75928188+Ajay12-p@users.noreply.github.com>
2023-05-30 10:49:00 +0530

    Update page.js

--- client/src/components/Pages/docs/page.js
+++ client/src/components/Pages/docs/page.js
@@ -1,9 +1,27 @@
+import Sidebar from "../../Sidebar/Sidebar";
+import "./docs.css"
 const Docs =()=>{
 
     return(
         <>
-        <div>
-            about this apps
+        <div className="mainDiv">
+            <div className="sidebar1">
+
+          <Sidebar/>
+            </div>
+          <div className="div2">
+           <div className="Heading">
+            How to use...
+
+           </div>
+           <div className="items1">
+            <img src="./docs1.png"/>
+           </div>
+           <div className="items1">
+            <img src="./docs2.png"/>
+           </div>
+          </div>
+
         </div>
         </>
     )
